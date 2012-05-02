378def855be1905b234413410f83c5daa3c38110
xxhfg <xxhfg@163.com>
2012-05-02 23:11:25 +0800

    abc

--- book/views.py
+++ book/views.py
@@ -5,9 +5,14 @@
 from django.http import HttpResponse
 from django.template import Context
 from django.template.loader import get_template
+from models import ContentInfo 
 
 def index(request):
+    """
+
+    """
     t = get_template('index.html')
-    html = t.render(Context({'title':'书库'}))
+    objs = ContentInfo.objects.all()
+    html = t.render(Context({'title':'书库', 'objects':objs}))
     return HttpResponse(html)
 
